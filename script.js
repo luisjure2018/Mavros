2027cd5a8019c999fb5482591179085985e1c6fd
luisjure2018 <39589023+luisjure2018@users.noreply.github.com>
2024-07-10 18:01:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-document.getElementById('blason-izq').addEventListener('click', iniciarAnimacion);
-document.getElementById('blason-der').addEventListener('click', iniciarAnimacion);
-
-function iniciarAnimacion() {
-    document.getElementById('blason-izq').classList.add('animate-left');
-    document.getElementById('blason-der').classList.add('animate-right');
-
-    setTimeout(function() {
-        document.getElementById('blason-container').style.display = 'none';
-        document.getElementById('contenido').style.display = 'block';
-    }, 1000);
-}
-
 document.querySelectorAll('.episodio-btn').forEach(function(button) {
     button.addEventListener('click', function() {
         var episodio = button.getAttribute('data-episodio');
