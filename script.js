a3fb152e53e469e3d0ea5ce717b3f2a75f47bb66
luisjure2018 <39589023+luisjure2018@users.noreply.github.com>
2024-07-10 17:14:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ document.getElementById('blason').addEventListener('click', function() {
     document.getElementById('contenido').style.display = 'block';
 });
 
-function mostrarEpisodio(numero) {
-    alert("Mostrar contenido del Episodio " + numero);
+document.querySelectorAll('.episodio-btn').forEach(function(button) {
+    button.addEventListener('click', function() {
+        var episodio = button.getAttribute('data-episodio');
+        cambiarEpisodio(episodio);
+    });
+});
+
+function cambiarEpisodio(numero) {
+    document.getElementById('titulo-personaje').style.display = 'none';
+    document.getElementById('contenido-episodio').style.display = 'block';
+    document.getElementById('titulo-episodio').textContent = 'Episodio ' + numero;
     // Aquí puedes añadir la lógica para mostrar el contenido del episodio seleccionado
 }
